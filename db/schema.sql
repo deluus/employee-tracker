5c274b3c99f33f2ff9fe3a7dd78d275de73b3404
deluus polycarp <pdeluus@gmail.com>
2022-03-05 15:57:37 -0800

    working on schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,29 @@
-DROP DATABASE IF EXISTS employee_db;
-CREATE DATABASE employee_db;
+DROP DATABASE IF EXISTS employee_DB;
+CREATE DATABASE employee_DB;
 
-USE employee_db;
+USE employee_DB;
 
 -- Create Tables for Department, Role, Employee
 CREATE Table department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT NOT NULL
     department_name VARCHAR(30) NOT NULL
     PRIMARY KEY(id)
 );
 
 CREATE Table roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL (10, 0) NOT NULL,
     department_id INT,
-    PRIMARY KEY(id)
+    -- PRIMARY KEY(id)
 );
 
 CREATE Table employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    PRIMARY KEY(id)
+    -- PRIMARY KEY(id)
 
 );
